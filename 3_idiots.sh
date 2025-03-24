7f5341d71c620137d6be04b3d315a71869d64007
Ubuntu <ubuntu@ip-172-31-11-156.ap-south-1.compute.internal>
2025-03-24 03:15:28 +0000

    added 3 idiots script

---
+++ 3_idiots.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+hero="rancho"
+villan="Virus"
+
+echo "3 idiots ka hero hai $hero"
+
+echo "3 idioits ka villan hai $villan"
+
+# shell / environment variable bhi hote hai
+(predefined)
+
+
+echo "current logged in user $USER"
+#user input
+read -p "rancho ka pura nam kya that" fullname
+
+echo "rancho la pura name $fullname tha"
+ # arguments
+
+ #./3_idiots.sh raju farhan rancho
+ 
+ echo "movie ka nam: $0"
+ echo "first idiot:$1"
+ echo "second idiot: $2"
+ echo "third idiot: $3"
+echo "the total number of idiots: $#"
+ echo "hence the 3 idiots are $@"
+
+
+
